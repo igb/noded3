03f0cc8cf737641e038f191d39bb911ae8876a83
ibrown <ibrown@twitter.com>
2021-04-23 05:29:48 -0700

    basic fill working

--- style.js
+++ style.js
@@ -1,7 +1,12 @@
 
- function getStyle(linkStrokeColor, linkStrokeOpacity, polygonFillColor, polygonStrokeColor, polygonStrokeWidth, sitesFillColor, sitesStrokeColor, sitesFillOpacity, sitesStrokeOpacity) {
+
+function getStyle(linkStrokeColor, linkStrokeOpacity, polygonFillColor, polygonStrokeColor, polygonStrokeWidth, sitesFillColor, sitesStrokeColor, sitesFillOpacity, sitesStrokeOpacity) {
+    return ".fill001 {\n\tfill: url(#circles-1);\n\tstroke: #000000;\n}\n";
+}
+
+/* function getStyle(linkStrokeColor, linkStrokeOpacity, polygonFillColor, polygonStrokeColor, polygonStrokeWidth, sitesFillColor, sitesStrokeColor, sitesFillOpacity, sitesStrokeOpacity) {
     return   ".links {\n\tstroke:" + linkStrokeColor + ";\n\tstroke-opacity: " + linkStrokeOpacity + ";\n}\n\n.polygons {\n\tfill: " + polygonFillColor + ";\n\tstroke: " +  polygonStrokeColor + ";\n\tstroke-width: " + polygonStrokeWidth + ";\n}\n\n.sites {\n\t stroke-opacity: " + sitesStrokeOpacity + ";\n\tfill-opacity: " + sitesFillOpacity + ";\n\tfill: " + sitesFillColor + ";\n\t stroke: " + sitesStrokeColor + ";\n}\n";
 
 }
-
+*/
 
